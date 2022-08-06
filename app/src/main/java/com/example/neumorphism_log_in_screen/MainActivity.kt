300fb7bf84b604c844c9082ae88de8fb1a8948f1
Nilavarasu2608 <knlsu444@gmail.com>
2022-08-06 12:38:46 +0530

    first commit

---
+++ app/src/main/java/com/example/neumorphism_log_in_screen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.neumorphism_log_in_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
