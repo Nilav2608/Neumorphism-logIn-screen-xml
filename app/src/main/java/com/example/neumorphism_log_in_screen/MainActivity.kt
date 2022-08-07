e6af9877b47ec93b658f7f853d486ad0b66f0ec5
Nilavarasu2608 <knlsu444@gmail.com>
2022-08-07 13:07:10 +0530

    first commit

--- app/src/main/java/com/example/neumorphism_log_in_screen/MainActivity.kt
+++ app/src/main/java/com/example/neumorphism_log_in_screen/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.neumorphism_log_in_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.concurrent.Delayed
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.signup_btn).setOnClickListener{
+            startActivity(Intent(this,MainActivity2::class.java))
+
+        }
+
     }
 
+
 }
+
---
+++ app/src/main/java/com/example/neumorphism_log_in_screen/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.neumorphism_log_in_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
